646b5d8a02c0f01c835c051f27b325aef2328272
juh9870 <juh9870@ukr.net>
2024-07-01 14:44:15 +0300

    build: fmt after fixes

--- fix.sh
+++ fix.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
-cargo fmt --all
 cargo fix --allow-dirty --allow-staged -q --all-features
 cargo clippy --fix --allow-dirty --allow-staged --all-features
+cargo fmt --all
 cargo sort -w
 cargo-machete --fix --skip-target-dir
